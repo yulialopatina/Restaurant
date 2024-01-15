40cc8148ff27786102a1e3829a239e7554c3ac03
Yulia Lopatina <sheridtest@gmail.com>
2024-01-15 14:33:07 +0300

    new commit

--- src/BaseDish.java
+++ src/BaseDish.java
@@ -2,20 +2,12 @@ public abstract class BaseDish {
     private String name;
     private double price;
 
-    BaseDish (String name, double price) {
-      this.name = name;
-      this.price = price;
+    BaseDish(String name, double price) {
+        this.name = name;
+        this.price = price;
     }
-    public String getName () {
+
+    public String getName() {
         return name;
-    }
-    public double getPrice () {
-        return mainDishPrice;
-    }
-    public void setMainDishname (String name) {
-        this.mainDishname = name;
-    }
-    public void setMainDishPrice (double price) {
-        this.mainDishPrice = price;
     }
 }
