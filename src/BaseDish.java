b9cfe2b34d38af09f71bc1d45914a3b26661d5dd
yulialopatina <144424713+yulialopatina@users.noreply.github.com>
2024-01-15 15:00:39 +0300

    Inheritance added (#1)

--- src/BaseDish.java
+++ src/BaseDish.java
@@ -2,12 +2,16 @@ public abstract class BaseDish {
     private String name;
     private double price;
 
-    BaseDish(String name, double price) {
+    public BaseDish(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
     public String getName() {
         return name;
+    }
+
+    public double getPrice() {
+        return price;
     }
 }
